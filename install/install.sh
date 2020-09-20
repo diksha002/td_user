133f683edc7bf1a45db4da34352b84db2ce454fc
diksha002 <61411982+diksha002@users.noreply.github.com>
2020-09-20 18:43:27 +0400

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -3,6 +3,8 @@
 cp ~/td_user/src/user.service /etc/systemd/system/
 echo "service created in /etc/systemd/system/"
 
+cd ~/etc/systemd/system
+
 sudo systemctl start work
 sudo systemctl enable work
 
